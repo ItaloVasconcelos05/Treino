4b16ae7c468fb14d5c3bc3aaab0b681da4dcdb8f
ItaloVasconcelos05 <163598100+ItaloVasconcelos05@users.noreply.github.com>
2024-03-22 15:14:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 75;
+console.log(numeroSecreto)
+let chute
+
+while (chute != numeroSecreto) {
+    
+    let chute = prompt('Escolha um número entre 1 e 100');
+    if (chute == numeroSecreto)
+    alert ('Parabéns!! Você acertou o número secreto ' + numeroSecreto)
+
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ` + chute);
+    } else {
+        alert(`O número secreto é maior que ` + chute);
+    }
+}
