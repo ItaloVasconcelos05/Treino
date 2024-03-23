cc6348954290d0e82fd6f2b2c8e717d54cfb8478
ItaloVasconcelos05 <163598100+ItaloVasconcelos05@users.noreply.github.com>
2024-03-23 19:46:11 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ console.log(numeroSecreto)
 let chute
 
 while (chute != numeroSecreto) {
-    
+    let tent = 1
+    tent++
     let chute = prompt('Escolha um número entre 1 e 100');
     if (chute == numeroSecreto)
     alert ('Parabéns!! Você acertou o número secreto ' + numeroSecreto)
@@ -15,3 +16,5 @@ while (chute != numeroSecreto) {
         alert(`O número secreto é maior que ` + chute);
     }
 }
+let tentativ = tent > 1 ? 'tentativas' : 'tentativa'
+alerta (`Você acertou o número Secreto ${nuneroSecreto} em ${tent} ${tentativ}`)
